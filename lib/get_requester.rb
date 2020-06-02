bf72d28b04ea43106e21f5888f877cca47b0e7b6
jonah <benjaminjmallard@gmail.com>
2020-06-02 17:55:46 -0500

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,19 +1,28 @@
-require 'net/http'
-require 'open-uri'
-require 'json'
+ require 'net/http'
+ require 'open-uri'
+ require 'json'
  
-class GetPrograms
- 
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
- 
-  def get_programs
+ class GetRequester
+  
+  def initialize(url)
+  end 
+
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+  def get_response_body
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end
- 
+
+  def parse_json
+    programs = JSON.parse(self.get_response_body)
+    programs.collect do |program|
+      program
+    end
+  end
+
 end
- 
-programs = GetPrograms.new.get_programs
-puts programs
- 
+
+# programs = GetPrograms.new.get_programs
+# puts programs
